cdcc4c790587458ae2f3ffcbd7867cf3a4b463c8
Elfrey Shira <elf.shira@gmail.com>
2018-11-03 17:28:06 +0700

    make buttons bigger

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const topics = _.keys(allTopics)
 
 function Button (props) {
   return (
-    <button className="button" {...props}>
+    <button className="hit-button" {...props}>
         {props.children}
     </button>
   )
