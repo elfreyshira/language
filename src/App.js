4a8e503f0ba71c9fd3d6f737568a5c1d50b14e52
Elfrey Shira <elf.shira@gmail.com>
2019-12-17 15:44:52 +0700

    can select/deselect sources

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-
 import _ from 'lodash';
 
 import allTopics from './resources/esl-questions/all-topics';
 import emotions from './resources/wheel-of-emotions/emotions';
+
+import './App.css';
 
 const topics = _.keys(allTopics)
 const isAdminMode =  window.location.search.match("mode=admin")
@@ -22,9 +22,9 @@ function getKey () {
 }
 
 
-function openWindowWithPost(url, data) {
+function openWindowWithPost(url, data, target) {
   const form = document.createElement("form");
-  form.target = "sinonimkata";
+  form.target = target;
   form.method = "POST";
   form.action = url;
   form.style.display = "none";
@@ -61,11 +61,54 @@ if (isAdminMode) {
 ///////////////////////////////////////////
 
 
+function Checkbox (props) {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" {...props} />
+        {props.children || props.label}
+      </label>
+    </div>
+  )
+}
+
+
+
+
+
+
+const websiteMapping = {
+  gtrans1: (searchValue) => window.open('https://translate.google.com.sg/?hl=en&client=tw-ob#view=home&op=translate&sl=id&tl=en&text=' + encodeURI(searchValue), 'gtrans1'),
+  kbbi: (searchValue) => window.open('https://kbbi.kemdikbud.go.id/entri/' + encodeURI(searchValue), 'kbbi'),
+  gtrans2: (searchValue) => window.open('https://translate.google.com.sg/?hl=en&client=tw-ob#view=home&op=translate&sl=id&tl=en&text=' + encodeURI(searchValue), 'gtrans2'),
+  
+  babla: (searchValue) => window.open('https://www.babla.co.id/bahasa-indonesia-bahasa-inggris/' + searchValue.split(' ').join('-'), 'babla'),
+  glosbe: (searchValue) => window.open('https://id.glosbe.com/id/en/' + encodeURI(searchValue) + '?stem=false', 'glosbe'),
+  jagokata: (searchValue) => window.open('https://jagokata.com/kata-bijak/kata-' + searchValue.split(' ').join('+') + '.html', 'jagokata'),
+  sinonimkata: (searchValue) => openWindowWithPost('http://www.sinonimkata.com/search.php', {q: searchValue}, 'sinonimkata'),
+  oxford: (searchValue) => window.open('https://id.oxforddictionaries.com/translate/indonesian-english/' + searchValue.split(' ').join('_'), 'oxford'),
+
+  alkitab: (searchValue) => window.open('https://alkitab.sabda.org/search.php?scope=all&exact=off&search=' + searchValue.split(' ').join('+'), 'alkitab'),
+  kitabsuci: (searchValue) => window.open('https://kitabsuci.mobi/kitabsuci/search/' + searchValue.split(' ').join('+'), 'kitabsuci'),
+
+  kitabgaul: (searchValue) => window.open('https://kitabgaul.com/word/' + searchValue.split(' ').join('-'), 'kitabgaul'),
+  twitter: (searchValue) => window.open('https://twitter.com/search?q=' + encodeURIComponent(searchValue + ' lang:id'), 'twitter'),
+
+  kompas: (searchValue) => window.open('https://search.kompas.com/search/?q=' + searchValue.split(' ').join('+'), 'kompas'),
+  kbbiui: (searchValue) => window.open('http://bahasa.cs.ui.ac.id/kbbi/kbbi.php?keyword=' + searchValue.split(' ').join('+') + '&varbidang=all&vardialek=all&varragam=all&varkelas=all&submit=tabel', 'kbbiui'),
+  }
+
+
+
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {selections: [], searchValue: ''}
+    this.state = {
+      selections: [],
+      searchValue: '',
+      urlsSelected: _.mapValues(websiteMapping, () => true)
+    }
   }
 
   getRandomQuestion () {
@@ -108,25 +151,11 @@ class App extends Component {
   }
 
   searchForWord (evt) {
-    const searchValue = this.state.searchValue
-
-    window.open('https://translate.google.com.sg/?hl=en&client=tw-ob#view=home&op=translate&sl=id&tl=en&text=' + encodeURI(searchValue), 'gtrans1')
-    window.open('https://kbbi.kemdikbud.go.id/entri/' + encodeURI(searchValue), 'kbbi')
-    window.open('https://translate.google.com.sg/?hl=en&client=tw-ob#view=home&op=translate&sl=id&tl=en&text=' + encodeURI(searchValue), 'gtrans2')
-    window.open('https://www.babla.co.id/bahasa-indonesia-bahasa-inggris/' + searchValue.split(' ').join('-'), 'babla')
-    window.open('https://id.glosbe.com/id/en/' + encodeURI(searchValue) + '?stem=false', 'glosbe')
-    window.open('https://jagokata.com/kata-bijak/kata-' + searchValue.split(' ').join('+') + '.html', 'jagokata')
-    openWindowWithPost('http://www.sinonimkata.com/search.php', {q: searchValue})
-    window.open('https://id.oxforddictionaries.com/translate/indonesian-english/' + searchValue.split(' ').join('_'), 'oxford')
-    
-    window.open('https://alkitab.sabda.org/search.php?scope=all&exact=off&search=' + searchValue.split(' ').join('+'), 'alkitab')
-    window.open('https://kitabsuci.mobi/kitabsuci/search/' + searchValue.split(' ').join('+'), 'kitabsuci')
-
-    window.open('https://kitabgaul.com/word/' + searchValue.split(' ').join('-'), 'kitabgaul')
-    window.open('https://twitter.com/search?q=' + encodeURIComponent(searchValue + ' lang:id'), 'twitter')
-    
-    window.open('https://search.kompas.com/search/?q=' + searchValue.split(' ').join('+'), 'kompas')
-    window.open('http://bahasa.cs.ui.ac.id/kbbi/kbbi.php?keyword=' + searchValue.split(' ').join('+') + '&varbidang=all&vardialek=all&varragam=all&varkelas=all&submit=tabel', 'kbbi-ui')
+    _.forEach(this.state.urlsSelected, (isChecked, source) => {
+      if (isChecked) {
+        websiteMapping[source](this.state.searchValue)
+      }
+    })
 
     evt.preventDefault()
   }
@@ -141,12 +170,23 @@ class App extends Component {
           onChange={(evt)=>this.setState({searchValue: evt.target.value})}
         />
         <div>
-          <label>
-            <input
-              type="checkbox"
-            />
-            Search multiple verb spellings
-          </label>
+          {_.map(this.state.urlsSelected, (value, key) => {
+            return (
+              <Checkbox
+                key={key}
+                checked={value}
+                label={key}
+                onChange={(evt) => {
+                  const clonedUrlsSelected = _.clone(this.state.urlsSelected)
+                  clonedUrlsSelected[key] = evt.target.checked
+                  this.setState({urlsSelected: clonedUrlsSelected})
+                }
+                }
+              />
+            )
+          })}
+
+
         </div>
         <Button type="submit" onClick={(evt)=>{this.searchForWord(evt); this.searchForWord(evt);}}>
           Search Word
